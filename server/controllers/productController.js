112ed4b63f6b46456c137271dc5f1b132698153b
Karmadipsinh  jadeja <karmadipsinhjadeja7@gmail.com>
2026-05-02 11:34:33 +0530

    Implement CRUD operations for products

---
+++ server/controllers/productController.js
@@ -0,0 +1,41 @@
+const Product = require("../models/Product");
+
+// GET ALL PRODUCTS (USER + ADMIN)
+exports.getProducts = async (req, res) => {
+  const products = await Product.find();
+  res.json(products);
+};
+
+// ADD PRODUCT (ADMIN)
+exports.addProduct = async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.json(product);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// UPDATE PRODUCT PRICE (ADMIN)
+exports.updateProduct = async (req, res) => {
+  try {
+    const updated = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.json(updated);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// DELETE PRODUCT (ADMIN)
+exports.deleteProduct = async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+    res.json({ message: "Product deleted" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
